4b93832a9a21b2219437473e5ecc298ddb569898
Arthur Ribeiro <arthursnr2021@gmail.com>
2024-02-06 16:43:54 -0300

    aula 02

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,55 +1,20 @@
 import fastify  from 'fastify';
-import { PrismaClient } from '@prisma/client';
-import { Poll } from '@prisma/client';
-import { z } from 'zod';
-import { request } from 'http';
+import cookie from '@fastify/cookie'
+import { createPoll } from './routes/create-poll';
+import { getPoll } from './routes/get-poll';
+import { voteOnPollPoll } from './routes/vote-on-poll';
 
 const app = fastify();
 
-const prisma = new PrismaClient()
-
-app.post('/polls', async (request, reply) => {
-    const createPollBody = z.object({
-        title: z.string()
-    })
-
-    const { title } = createPollBody.parse(request.body)
-
-    const poll = await prisma.poll.create({
-        data: {
-            title,
-        }
-    })
-
-    return reply.status(201).send({ pollId: poll.id })
+app.register(cookie, {
+    secret: "polls-app-nlw",
+    hook: 'onRequest',
+    parseOptions: {}
 })
 
-app.get('/polls', (request) => {
-    console.log(request.body)
-
-    return'Hello NLW'
-})
-
-app.delete('/polls', async (request, reply) => {
-    const deletePollBody = z.object({
-        id: z.string()
-    });
-
-    const { id } = deletePollBody.parse(request.body);
-
-    try {
-        const deletedPoll = await prisma.poll.delete({
-            where: {
-                id,
-            },
-        });
-
-        reply.status(200).send({ message: 'Poll deleted successfully', poll: deletedPoll });
-    } catch (error) {
-        console.error('Error deleting poll:', error);
-        reply.status(500).send({ message: 'Internal Server Error' });
-    }
-});
+app.register(createPoll)
+app.register(getPoll)
+app.register(voteOnPollPoll)
 
 app.listen({ port: 3333 }).then(() => {
     console.log('HTTP server running!')
